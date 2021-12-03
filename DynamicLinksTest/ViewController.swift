58605460834d2cb0450dad4caf4c0cd56c759273
kazuya sasakura <dropout.college2014@gmail.com>
2021-12-03 18:24:55 +0900

    Initial Commit

---
+++ DynamicLinksTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DynamicLinksTest
+//
+//  Created by 笹倉一也 on 2021/12/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
